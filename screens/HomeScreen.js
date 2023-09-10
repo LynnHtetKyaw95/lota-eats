aa5e072daa60cf270777b92a3ed52393eb66940a
Zephyrl <dklin1995@gmail.com>
2023-09-10 14:44:49 +0630

    Second Component: SearchBar.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <HeaderTabs />
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
     </SafeAreaView>
   );
 };
